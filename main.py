d2302b85136b40ee4ed0459f2dcfb9e61b9b89c7
AlexBarthel <98185865+AlexBarthel@users.noreply.github.com>
2024-06-28 16:32:35 -0500

    implementing arrow key navigation in main menu

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ op_btn_bg = PhotoImage(file="img/options.png")
 op_btn_bg_alt = PhotoImage(file="img/options-alt.png")
 
 # create buttons on main screen
-menu_btn_1 = Button(window, image=sm_btn_bg, bg="black", borderwidth=0)
+menu_btn_1 = Button(window, image=sm_btn_bg_alt, bg="black", borderwidth=0)
 menu_btn_1.grid(row=0, column=0)
 
 menu_btn_2 = Button(window, image=fp_btn_bg, bg="black", borderwidth=0)
@@ -29,6 +29,57 @@ window.grid_rowconfigure(0, weight=1)
 window.grid_rowconfigure(1, weight=1)
 window.grid_rowconfigure(2, weight=1)
 window.grid_columnconfigure(0, weight=1)
+
+# initialize the selected button
+selected_btn = 0
+previous_btn = None
+
+def update_btn_states():
+    if selected_btn == 0:
+        menu_btn_1.config(image=sm_btn_bg_alt)
+        menu_btn_2.config(image=fp_btn_bg)
+        menu_btn_3.config(image=op_btn_bg)
+    elif selected_btn == 1:
+        menu_btn_1.config(image=sm_btn_bg)
+        menu_btn_2.config(image=fp_btn_bg_alt)
+        menu_btn_3.config(image=op_btn_bg)
+    elif selected_btn == 2:
+        menu_btn_1.config(image=sm_btn_bg)
+        menu_btn_2.config(image=fp_btn_bg)
+        menu_btn_3.config(image=op_btn_bg_alt)
+
+def arrow_up_handler(e):
+    global previous_btn
+    global selected_btn
+    
+    # set the previous btn to the current btn index
+    previous_btn = selected_btn
+
+    # find the next btn index
+    selected_btn -= -2 if selected_btn == 0 else 1
+
+    # change the state of the btns
+    update_btn_states()
+
+def arrow_down_handler(e):
+    global previous_btn
+    global selected_btn
+    
+    # set the previous btn to the current btn index
+    previous_btn = selected_btn
+
+    # find the next btn index
+    selected_btn += -2 if selected_btn == 2 else 1
+
+    # change the state of the btns
+    update_btn_states()
+
+def return_handler(e):
+    print("STORY MODE" if selected_btn == 0 else "FREEPLAY" if selected_btn == 1 else "OPTIONS")
+
+window.bind("<Up>", arrow_up_handler)
+window.bind("<Down>", arrow_down_handler)
+window.bind("<Return>", return_handler)
 
 # display the tk window
 window.mainloop()
