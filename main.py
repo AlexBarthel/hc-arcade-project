7732e83ee31dcf3c222ca04759f98eb43efd46c6
AlexBarthel <98185865+AlexBarthel@users.noreply.github.com>
2024-06-28 15:56:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,12 +16,19 @@ op_btn_bg = PhotoImage(file="img/options.png")
 op_btn_bg_alt = PhotoImage(file="img/options-alt.png")
 
 # create buttons on main screen
-story_mode_btn = Button(window, image=sm_btn_bg)
-story_mode_btn.pack()
-freeplay_btn = Button(window, image=fp_btn_bg)
-freeplay_btn.pack()
-options_btn = Button(window, image=op_btn_bg, bg=None)
-options_btn.pack()
+menu_btn_1 = Button(window, image=sm_btn_bg, bg="black", borderwidth=0)
+menu_btn_1.grid(row=0, column=0)
+
+menu_btn_2 = Button(window, image=fp_btn_bg, bg="black", borderwidth=0)
+menu_btn_2.grid(row=1, column=0)
+
+menu_btn_3 = Button(window, image=op_btn_bg, bg="black", borderwidth=0)
+menu_btn_3.grid(row=2, column=0)
+
+window.grid_rowconfigure(0, weight=1)
+window.grid_rowconfigure(1, weight=1)
+window.grid_rowconfigure(2, weight=1)
+window.grid_columnconfigure(0, weight=1)
 
 # display the tk window
 window.mainloop()
