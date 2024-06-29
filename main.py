30fdee8e70fd66c328f7dccaab4ec2f38c06055c
AlexBarthel <98185865+AlexBarthel@users.noreply.github.com>
2024-06-29 17:05:50 -0500

    add menu music & use enter key to switch scenes

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from tkinter import *
-from playsound import playsound
+import winsound
 
 # configure the window
 window = Tk()
 window.title("hc-arcade-project")
 window.attributes("-fullscreen", True)
 window.configure(background="black")
+window.configure(cursor="none")
 
 # define images
 sm_btn_bg = PhotoImage(file="img/storymode.png")
@@ -16,13 +17,13 @@ op_btn_bg = PhotoImage(file="img/options.png")
 op_btn_bg_alt = PhotoImage(file="img/options-alt.png")
 
 # create buttons on main screen
-menu_btn_1 = Button(window, image=sm_btn_bg_alt, bg="black", borderwidth=0)
+menu_btn_1 = Button(window, image=sm_btn_bg_alt, bg="black", borderwidth=0, activebackground="black")
 menu_btn_1.grid(row=0, column=0)
 
-menu_btn_2 = Button(window, image=fp_btn_bg, bg="black", borderwidth=0)
+menu_btn_2 = Button(window, image=fp_btn_bg, bg="black", borderwidth=0, activebackground="black")
 menu_btn_2.grid(row=1, column=0)
 
-menu_btn_3 = Button(window, image=op_btn_bg, bg="black", borderwidth=0)
+menu_btn_3 = Button(window, image=op_btn_bg, bg="black", borderwidth=0, activebackground="black")
 menu_btn_3.grid(row=2, column=0)
 
 window.grid_rowconfigure(0, weight=1)
@@ -75,11 +76,17 @@ def arrow_down_handler(e):
     update_btn_states()
 
 def return_handler(e):
+    winsound.PlaySound(None, winsound.SND_PURGE)
+    menu_btn_1.grid_forget()
+    menu_btn_2.grid_forget()
+    menu_btn_3.grid_forget()
     print("STORY MODE" if selected_btn == 0 else "FREEPLAY" if selected_btn == 1 else "OPTIONS")
 
 window.bind("<Up>", arrow_up_handler)
 window.bind("<Down>", arrow_down_handler)
 window.bind("<Return>", return_handler)
+
+winsound.PlaySound("audio\menu-music.wav", winsound.SND_ASYNC | winsound.SND_LOOP)
 
 # display the tk window
 window.mainloop()
